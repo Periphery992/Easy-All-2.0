13b05ed48a7e2cb33f13ec8b1ef2c5441ed21b5d
wind4app <chensa@wind4app.com>
2018-06-01 22:06:30 +0800

    添加Account模块

---
+++ Easy-All 2.0/Easy-All 2.0/Account/Home/AccountHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountHomeViewController.h
+//  Easy-All 2.0
+//
+//  Created by chensa on 2018/5/31.
+//  Copyright © 2018年 陈舒澳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountHomeViewController : UIViewController
+
+@end
---
+++ Easy-All 2.0/Easy-All 2.0/Account/Home/AccountHomeViewController.m
@@ -0,0 +1,29 @@
+//
+//  AccountHomeViewController.m
+//  Easy-All 2.0
+//
+//  Created by chensa on 2018/5/31.
+//  Copyright © 2018年 陈舒澳. All rights reserved.
+//
+
+#import "AccountHomeViewController.h"
+
+@implementation AccountHomeViewController
+
+#pragma mark - LifeCycle Methods
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+#pragma mark - Delegate Methods
+
+#pragma mark - Public Methods
+
+#pragma mark - Private Methods
+
+#pragma mark - Layout Methods
+
+#pragma mark - GET/SET Methods
+@end
